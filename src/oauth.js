10d50a4fd87693622cc0d564b3a260e71751332a
Thomas Getgood <thomas.getgood@gmail.com>
2017-11-26 13:09:54 -0500

    refactor: oauth code cleaning.

--- src/oauth.js
+++ src/oauth.js
@@ -11,35 +11,34 @@ const state = () => '' + rando() + 'v' + rando()
 
 const oauthKey = 'maintainer-github-auth'
 
-// let githubToken = window.localStorage.getItem(oauthKey)
+let githubToken = window.localStorage.getItem(oauthKey)
 
-// const query = require('query-string').parse(window.location.search)
+const query = require('query-string').parse(window.location.search)
 
-// const authenticate = () => new Promise((resolve, reject) => {
-//   if (githubToken) {
-//     resolve(githubToken)
-//   } else if (query.code) {
-//     if (query.state === window.localStorage.getItem('state')) {
-//       return http({
-//         method: 'post',
-//         headers: {'Accept': 'application/json'},
-//         url: 'https://github.com/login/oauth/access_token',
-//         params: {
-//           client_id: clientID,
-//           client_secret: '',
-//           code: query.code
-//         }})
-//     } else {
-//       reject(new Error('Possible CSRF attack. Denied.'))
-//     }
-//   } else {
-//     const theState = state()
-//     window.localStorage.setItem('state', theState)
-//     window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&state=${theState}`
-//   }
-// })
+const authenticate = () => new Promise((resolve, reject) => {
+  if (githubToken) {
+    resolve(githubToken)
+  } else if (query.code) {
+    if (query.state === window.localStorage.getItem('state')) {
+      return http({
+        method: 'post',
+        headers: {'Accept': 'application/json'},
+        url: 'https://github.com/login/oauth/access_token',
+        params: {
+          client_id: clientID,
+          client_secret: '',
+          code: query.code
+        }})
+    } else {
+      reject(new Error('Possible CSRF attack. Denied.'))
+    }
+  } else {
+    const theState = state()
+    window.localStorage.setItem('state', theState)
+    window.location = `https://github.com/login/oauth/authorize?client_id=${clientID}&state=${theState}`
+  }
+})
 
 module.exports = {
-  // authenticate,
-  dummyAuthenticate: () => new Promise((resolve, reject) => resolve(require('../dummyAuth')))
+  authenticate
 }
